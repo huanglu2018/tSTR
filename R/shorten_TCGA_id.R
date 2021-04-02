8db3be761d14ea20ce97c4640211b454da8e1eb8
huanglu2018 <705122613@qq.com>
2021-04-02 04:10:42 +0000

    first function

---
+++ R/shorten_TCGA_id.R
@@ -0,0 +1,26 @@
+#' used to conveniently cut TCGA sample IDs
+#'
+#' @param id_list individual list, e.g. TCGA-AAAA-BBBB
+#' @param left_number how many parts (separated by '-' to keep, should not be longer than its original length )
+#'
+#' @return a shortened list
+#' @export
+#'
+#' @examples
+#'
+
+
+shorten_TCGA_id=function(id_list,left_number){
+
+  library(data.table)
+  library(tidyr)
+  library(magrittr)
+  library(dplyr)
+
+  s=id_list %>% as.data.frame() %>%
+    set_colnames(c("all")) %>%
+    separate(.,1,paste0("V",1:7),"-") %>%
+    unite(x, c(paste0("V",1:as.numeric(left_number))), sep = "-", remove = FALSE,fill="left") %>%
+     .$x
+  return(s)
+}
